e198f4631564569caa4c0238bb1b81f77490adb0
DaryaTolkien <tolkien234@mail.ru>
2018-12-11 14:46:12 +0300

    )

--- basic/views/activity/form-success.php
+++ basic/views/activity/form-success.php
@@ -13,5 +13,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <h1>Название события: <?= Html::encode($model->name)?> </h1>
 <h1>Описание события: <?= Html::encode($model->description)?> </h1>
+
 <h1>Фото события: <img src="/images/img_event/<?= Html::encode($model->imageFile)?>" style="width:250px; height=:250px;"> </h1>
 
